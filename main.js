f3f5cea47878459e1ae9cecca10128900f14c7bc
Lionel <l.privatleblon@gmail.com>
2023-01-25 16:32:52 +0100

    Sauvegarde

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+function ajoutpanier() {
+  const panier = {
+    departure: "toto",
+    arrival: "toto",
+    date: 2012 - 01,
+    hour: 12.0,
+    price: 15,
+  };
+  for (let i = 0; i < document.querySelectorAll(".achat").length; i++) {
+    document
+      .querySelectorAll(".achat")
+      [i].addEventListener("click", function () {
+        fetch(`http://localhost:3000/checkings`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(panier),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.result) {
+              window.location.assign("index.html");
+              //this.parentNode.remove();
+            }
+          });
+      });
+  }
+}
+
 document.querySelector("#search").addEventListener("click", function () {
   const trip = {
     departure: document.querySelector("#departure").value,
@@ -19,11 +47,12 @@ document.querySelector("#search").addEventListener("click", function () {
         document
           .querySelector("#data-result")
           .classList.remove("no-result-trip-container");
+        document.querySelector("#data-result").textContent = "";
         document.querySelector("#data-result").innerHTML += `
 			<div>
         <div class="one-trip-container">
         <p>${data.trips.departure} / ${data.trips.arrival} le ${data.newDate} à ${data.newHour} ${data.trips.price}€<p>
-        <button id="book">book</button>
+        <button id="${data.trips._id}" type="submit" value="Send Request" class="achat">book</button>
         </div>
         </div>`;
         console.log(data);
@@ -33,6 +62,7 @@ document.querySelector("#search").addEventListener("click", function () {
         /*if (data.result) {
         window.location.assign("index.html");
       }*/
+        ajoutpanier();
       } else {
         document
           .querySelector("#data-result")
@@ -40,6 +70,7 @@ document.querySelector("#search").addEventListener("click", function () {
         document
           .querySelector("#data-result")
           .classList.remove("result-trip-container");
+        document.querySelector("#data-result").textContent = "No trip found";
       }
     });
 });

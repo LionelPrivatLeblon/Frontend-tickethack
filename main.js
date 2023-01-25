df3712894e5e8bdf07d6386a8d1f28441b2d7a72
Lionel <l.privatleblon@gmail.com>
2023-01-25 10:32:51 +0100

    Sauvegarde

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 document.querySelector("#search").addEventListener("click", function () {
   const trip = {
     departure: document.querySelector("#departure").value,
-    //email: document.querySelector("#arrival").value,
-    //password: document.querySelector("#selectDate").value,
+    arrival: document.querySelector("#arrival").value,
+    selectDate: document.querySelector("#selectDate").value,
   };
 
-  fetch("http://localhost:3000/trips/departure/", {
+  fetch("http://localhost:3000/trips/departure", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(trip),
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      /*if (data.result) {
+      if (data.result) {
+        document.querySelector("#data-result").innerHTML += `
+			<div>
+      <p>${data.trips.departure}</p>
+				<p>${data.trips.arrival}</p>
+				<p>${data.newDate}</p>
+        <p>${data.newHour}</p>
+				<p>${data.trips.price}</p>
+			</div>`;
+        console.log(data);
+        document.querySelector("#departure").value = "";
+        document.querySelector("#arrival").value = "";
+        document.querySelector("#selectDate").value = "";
+        /*if (data.result) {
         window.location.assign("index.html");
       }*/
+      }
     });
 });

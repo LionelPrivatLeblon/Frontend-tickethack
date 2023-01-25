f1d6ed2efb30ee70fe3e75a1a0db4d8f7f14f437
Lionel <l.privatleblon@gmail.com>
2023-01-25 09:19:53 +0100

    Sauvegarde

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 document.querySelector("#search").addEventListener("click", function () {
   const trip = {
-    name: document.querySelector("#departure").value,
+    departure: document.querySelector("#departure").value,
     //email: document.querySelector("#arrival").value,
     //password: document.querySelector("#selectDate").value,
   };
 
-  fetch("http://localhost:3000/departure/", {
+  fetch("http://localhost:3000/trips/departure/", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(trip),
   })
     .then((response) => response.json())
     .then((data) => {
-      if (data.result) {
+      console.log(data);
+      /*if (data.result) {
         window.location.assign("index.html");
-      }
+      }*/
     });
 });
